797750276b5d8f496c5ea5e065344dc85cb3cc21
utavegu <utavegu@list.ru>
2020-03-14 17:33:55 +0500

    Добавил возможность открытия / закрытия формы разными способами

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@
 /*
 НЕЗАКОНЧЕННЫЕ ЗАДАНИЯ
 
+!Сделай корректные имена ДОМ-элементов! - не буду пока, пожалуй, это делать. Всмысле на этом интенсиве.
+
 3-2:
 1) В каждом рандомном объекте должно быть от 1 до 3 рандомных комментариев, не повторяющихся! (вот тут видимо сплайс-слайс пригодится). Имена авторов и коомментарии так же случайные.
 2) Сообрази чонибудь с дескрипшинами. Пусть это будет дата, например. Массив дат. Без заморочек - просто строка.
@@ -103,6 +105,7 @@ picturesContainer.appendChild(fragment);
 /* ------------------------------------------------------ */
 
 // БЛОК ПОКАЗА БОЛЬШОГО ИЗОБРАЖЕНИЯ
+/*
 var bigPicture = document.querySelector('.big-picture');
 bigPicture.classList.remove('hidden');
 bigPicture.querySelector('.big-picture__img').children[0].src = photoDescription[0].url;
@@ -113,3 +116,73 @@ bigPicture.querySelector('.social__caption').textContent = photoDescription[0].d
 
 document.querySelector('.social__comment-count').classList.add('visually-hidden'); // Убрал счётчик комментариев
 document.querySelector('.comments-loader').classList.add('visually-hidden'); // Убрал "Загрузить ещё"
+*/
+
+/* ------------------------------------------------------ */
+
+// 4-2
+
+var ENTER_KEYCODE = 13;
+var ESC_KEYCODE = 27;
+var SPACE_KEYCODE = 32; // Вроде... не тестил.
+
+// Форма редактирования изображения
+var imageEditingForm = document.querySelector('.img-upload__overlay');
+
+// Контрол загрузки файла
+var fileUploadControl = document.querySelector('#upload-file');
+
+// Крестик для закрытия формы редактирования изображения
+var formCloseCross = imageEditingForm.querySelector('.img-upload__cancel');
+
+
+// Функция показа формы редактирования изображения
+var showImageEditingForm = function () {
+  imageEditingForm.classList.remove('hidden');
+  fileUploadControl.removeEventListener('click', onControlChange);
+  formCloseCross.addEventListener('click', onCrossClick);
+  document.addEventListener('keydown', onEscPress);
+};
+
+// Функция закрытия формы редактирования изображения
+var hideImageEditingForm = function () {
+  imageEditingForm.classList.add('hidden');
+  formCloseCross.removeEventListener('click', onCrossClick);
+  document.removeEventListener('keydown', onEscPress);
+  fileUploadControl.addEventListener('click', onControlChange);
+  // fileUploadControl.value...??? КОРОЧЕ СБРОСИТЬ ЗНАЧЕНИЕ
+  /*При написании обработчиков, реагирующих на закрытие формы, обратите внимание на то, что при закрытии формы, дополнительно необходимо сбрасывать значение поля выбора файла #upload-file. В принципе, всё будет работать, если при повторной попытке загрузить в поле другую фотографию, но событие change не сработает, если вы попробуете загрузить ту же фотографию.*/
+};
+
+
+// Обработчик смены контрола (показывает форму)
+var onControlChange = function (evt) {
+  evt.preventDefault()  // ВОТ ЭТО ТОЖЕ ПОТОМ УБЕРИ. И ЕВТ ИЗ ПАРАМЕТРА
+  showImageEditingForm();
+};
+
+// Обработчик клика по крестику закрытия (закрывает форму)
+var onCrossClick = function () {
+  hideImageEditingForm();
+};
+
+// Обработчик нажатия ESC (закрывает форму)
+var onEscPress = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    hideImageEditingForm();
+  }
+};
+
+
+// Слушатель события чейндж на контроле загрузки файла
+fileUploadControl.addEventListener('click', onControlChange);  // ДЛЯ ПРОСТОТЫ ПОКА ПОМЕНЯЮ change на click
+
+// Слушатель события клик на крестике закрытия формы
+formCloseCross.addEventListener('click', onCrossClick);
+
+// Слушатель нажатия ескейп на документе
+document.addEventListener('keydown', onEscPress);
+
+// НА ДАННОМ ЭТАПЕ ИЗ БАГОВ - ПРИ ОТКРЫТОЙ ФОРМЕ (если конрол в фокусе) НАЖАТИЕ НА ЭНТЕР ИЛИ ПРОБЕЛ ПРИВОДИТ К ОТКРЫТИЮ ИНТЕРФЕЙСА ЗАГРУЗКИ ФАЙЛА - В БУДУЩЕМ НЕ ЗАБУДЬ ПРО ЭТО.
+
+/* добавим на пин слайдера .effect-level__pin обработчик события mouseup, который будет согласно ТЗ изменять уровень насыщенности фильтра для изображения. Для определения уровня насыщенности, нужно рассчитать положение пина слайдера относительно всего блока и воспользоваться пропорцией, чтобы понять, какой уровень эффекта нужно применить. */
