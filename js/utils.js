150524f2fab2db1507ed986104b6ee7178c03cf8
utavegu <utavegu@list.ru>
2020-06-13 13:28:50 +0500

    Еще немного порефакторил

--- js/utils.js
+++ js/utils.js
@@ -1,10 +1,14 @@
-const getRandomInteger = (min, max) => {
+// export const a = 1;
+
+/*
+export const getRandomInteger = (min, max) => {
   return min + Math.floor(Math.random() * (max - min));
 };
 
-const getRandomArrayItem = (array) => {
+export const getRandomArrayItem = (array) => {
   const randomIndex = getRandomInteger(0, array.length);
   return array[randomIndex];
 };
+*/
 
-export {getRandomInteger, getRandomArrayItem};
+// alert("Я загрузился!");
